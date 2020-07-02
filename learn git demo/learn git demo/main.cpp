a2cf04283bbeac5d0ebd911939463445fc8ede26
tubefox <2846552323@qq.com>
2020-07-02 13:33:20 +0800

    First Commit

---
+++ learn git demo/learn git demo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  learn git demo
+//
+//  Created by Smith Max on 2020/7/2.
+//  Copyright © 2020 Smith Max. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
